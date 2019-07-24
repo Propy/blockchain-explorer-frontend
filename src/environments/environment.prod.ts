953f0876e4bee7e4089f967c681381d92fb85ccc
Manol Trendafilov <m.trendafilov@propy.com>
2019-07-24 11:02:59 +0300

    add environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  transactions: {
+    page: 1,
+    limit: 50
+  },
+  identityServer: {
+    url: 'https://dev.api.identityserver.propy.com',
+    endpoints: {
+      GET_TOKEN: '/connect/token',
+      REGISTER_USER: '/api/v3/users/register',
+      REFRESH_TOKEN: '/connect/token',
+      GET_USER: '/Users/single'
+    }
+  },
+  clientId: '',
+  clientSecret: '',
+  china: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,24 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  transactions: {
+    page: 1,
+    limit: 50,
+    searchLimit: 20
+  },
+  identityServer: {
+    url: 'https://dev.api.identityserver.propy.com',
+    endpoints: {
+      GET_TOKEN: '/connect/token',
+      REGISTER_USER: '/api/v3/users/register',
+      REFRESH_TOKEN: '/connect/token',
+      GET_USER: '/Users/single'
+    }
+  },
+  clientId: '',
+  clientSecret: '',
+  china: false
 };
 
 /*
