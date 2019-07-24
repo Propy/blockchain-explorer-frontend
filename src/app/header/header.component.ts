8a38f4e2808865bc2b0831fa65e4b9475bd750e4
Manol Trendafilov <m.trendafilov@propy.com>
2019-07-24 11:08:49 +0300

    header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,63 @@
+
+import {Observable} from 'rxjs';
+import {NgbTypeahead} from '@ng-bootstrap/ng-bootstrap';
+import {debounceTime, distinctUntilChanged, map, switchMap} from 'rxjs/operators';
+import {TransactionsService} from '../shared/transactions.service';
+import {Transaction} from '../shared/models/transaction';
+import {TransactionData} from '../shared/interfaces/transaction-data';
+import {environment} from '../../environments/environment';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {NgForm} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  model: any;
+  limit: number = environment.transactions.searchLimit;
+  transactions: Transaction[];
+  clickedItem: any;
+  @ViewChild('instance', {static: true}) instance: NgbTypeahead;
+  @ViewChild('searchForm', {static: true}) form: NgForm;
+
+
+  constructor(private transactionsService: TransactionsService, private router: Router, private route: ActivatedRoute) { }
+
+  search = (text$: Observable<string>) => {
+    return text$.pipe(
+        debounceTime(200),
+        distinctUntilChanged(),
+        switchMap((searchText) => this.loadData(searchText))
+    );
+  }
+
+
+  loadData = (keyword: any) => {
+    return this.transactionsService.search(keyword, this.limit).pipe(map( (data: TransactionData) => {
+      return data.items;
+    }));
+  }
+
+  submit = (form) => {
+    this.router.navigate(['/transaction', typeof form.value.model === 'object' ? form.value.model.id : form.value.model], {
+      queryParams: {refresh: new Date().getTime()}
+    });
+  }
+
+  formatter = (x: {id: string}) => x.id;
+
+  selectedItem = (item) => {
+    this.clickedItem = item;
+    this.form.value.model = item.item;
+    this.form.ngSubmit.emit();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
