d9cb9c990d87dd9fe50924f6811b843922192963
Manol Trendafilov <m.trendafilov@propy.com>
2019-07-24 15:28:16 +0300

    footer add menus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TransactionDetailsComponent } from './transaction-details/transaction-details.component';
-import {TransactionListComponent} from './transaction-list/transaction-list.component';
-import {AboutComponent} from './pages/about/about.component';
-import {ContactComponent} from './pages/contact/contact.component';
-import {TermsComponent} from './pages/terms/terms.component';
-import {WatchlistComponent} from './watchlist/watchlist.component';
-import {LoginComponent} from './login/login.component';
 
-const routes: Routes = [
-  { path: 'transaction/:id', component: TransactionDetailsComponent },
-  { path: 'transactions', component: TransactionListComponent },
-  { path: 'about', component: AboutComponent},
-  { path: 'contact', component: ContactComponent},
-  { path: 'terms', component: TermsComponent},
-  { path: 'watchlist', component: WatchlistComponent},
-  { path: 'login', component: LoginComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
