9dda99cbd18da755f6396ed940b91f60a675ded4
Manol Trendafilov <m.trendafilov@propy.com>
2019-07-25 10:12:38 +0300

    add route for transaction details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import {ContactComponent} from './pages/contact/contact.component';
 import {AboutComponent} from './pages/about/about.component';
 import {LoginComponent} from './login/login.component';
 import {TransactionListComponent} from './transaction-list/transaction-list.component';
+import {TransactionDetailsComponent} from './transaction-details/transaction-details.component';
 
 const routes: Routes = [
     { path: 'about', component: AboutComponent},
     { path: 'contact', component: ContactComponent},
     { path: 'terms', component: TermsComponent},
     { path: 'login', component: LoginComponent},
-    { path: 'transactions', component: TransactionListComponent }
+    { path: 'transactions', component: TransactionListComponent },
+    { path: 'transaction/:id', component: TransactionDetailsComponent }
 ];
 
 @NgModule({
